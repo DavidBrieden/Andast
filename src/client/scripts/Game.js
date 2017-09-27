cddb6f6402b2b1d9f2f574f47e99f35fe5a87acc
Stephen Brieden <stephenbrieden@gmail.com>
2017-09-27 18:20:40 +0200

    Made boxes turnable

--- src/client/scripts/Game.js
+++ src/client/scripts/Game.js
@@ -25,6 +25,33 @@ Game.start = function(){
             // up arrow
             // Game.movePlayer(0,1);
             Game.spawn();
+        }else if (e.keyCode == '32') {
+            // press space to flip boxes
+            e.preventDefault();
+            for (var i=0;i<4;i++){
+                // Koordinate im Spielfeld setzen
+                Field.field[Game.player.position.x - Game.player.figure[i].x][Game.player.position.y - Game.player.figure[i].y] = {
+                show: false,
+                color: "green"
+            };
+    }  
+            // var flip = Array.from(Game.player.figure);
+            var flip1 = Game.player.figure[0];
+            var flip2 = Game.player.figure[1];
+            var flip3 = Game.player.figure[2];
+            var flip4 = Game.player.figure[3];
+            
+            Game.player.figure[0]={x:0-flip1.y,y:flip1.x};
+            Game.player.figure[1]={x:0-flip2.y,y:flip2.x};
+            Game.player.figure[2]={x:0-flip3.y,y:flip3.x};
+            Game.player.figure[3]={x:0-flip4.y,y:flip4.x};
+
+            /*for (var i = 0; i<4;i++){
+                flip[i].x = 1 - Game.player.figure[i].y;
+                flip[i].y = Game.player.figure[i].x;
+            }*/
+            //Game.player.figure = flip;
+            Game.movePlayer(0,0);
         }
     });
 
@@ -82,8 +109,8 @@ Game.movePlayer = function(x,y){
     Game.player.position.y+=y;
     Game.player.position.x+=x;
     // Jede Koordinate der gewählten Figur iterieren
+    // Koordinate im Spielfeld setzen
     for (var i=0;i<4;i++){
-        // Koordinate im Spielfeld setzen
         Field.field[Game.player.position.x - Game.player.figure[i].x][Game.player.position.y - Game.player.figure[i].y] = {
             show: true,
             color: Game.player.color
