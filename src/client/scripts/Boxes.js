7709a73e883b6b2752c039e76ab5802edd4f036b
Stephen Brieden <stephenbrieden@gmail.com>
2017-09-27 18:38:40 +0200

    Added more forms

--- src/client/scripts/Boxes.js
+++ src/client/scripts/Boxes.js
@@ -2,9 +2,24 @@ var boxes = [];
 
 // X
 //XXX
-boxes.push([{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:1,y:1}]);
+boxes.push([{x:-1,y:0},{x:0,y:0},{x:1,y:0},{x:0,y:1}]);
 //XX
 // XX
-boxes.push([{x:1,y:0},{x:0,y:1},{x:0,y:2},{x:1,y:1}]);
+boxes.push([{x:-1,y:1},{x:0,y:1},{x:0,y:0},{x:1,y:0}]);
+//XX
+//XX
+boxes.push([{x:0,y:0},{x:0,y:1},{x:1,y:0},{x:1,y:1}]);
+//X
+//X
+//X
+//X
+boxes.push([{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:0,y:-1}]);
+//X
+//X
+//XX
+boxes.push([{x:0,y:0},{x:0,y:1},{x:0,y:-1},{x:-1,y:-2}]);
+// XX
+//XX
+boxes.push([{x:-1,y:0},{x:0,y:0},{x:0,y:1},{x:1,y:1}]);
 
 module.exports = boxes;
