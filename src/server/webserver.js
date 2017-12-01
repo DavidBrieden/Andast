e4a39c6a52404d2302de0f320e753b174a303e75
David Brieden <davidbrieden@aol.com>
2017-12-01 18:27:11 +0100

    added support for autostart in plesk

--- src/server/webserver.js
+++ src/server/webserver.js
@@ -6,7 +6,7 @@ var start = function() {
     
     app.use(express.static('src/client/wwwroot'));
 
-    http.listen(3000, function(){
+    http.listen(process.env.PORT || 3000, function(){
         console.log('listening on *:3000');
     });
 
